2c412035bbd5a46594a81ba893e0c3fff9b2897c
Alex Plescan <alexpls@gmail.com>
2016-04-25 18:11:19 +1000

    Louder in some places, quieter in others

--- script/deploy_gh_pages.sh
+++ script/deploy_gh_pages.sh
@@ -9,6 +9,8 @@ fi
 
 GH_PAGES_PATH="$TRAVIS_BUILD_DIR/../gh_pages_repo"
 
+echo "Cloning existing repo..."
+
 git clone \
   "https://${GH_ACCESS_TOKEN}@${GH_PAGES_PRODUCTION_REF}" \
   $GH_PAGES_PATH
@@ -20,8 +22,14 @@ git checkout gh-pages
 git config user.name 'Alex Plescan'
 git config user.email 'alexpls@gmail.com'
 
+echo "Syncing across changes..."
+
 rsync --exclude ".git" --recursive --checksum --delete "$TRAVIS_BUILD_DIR/_site/" "$GH_PAGES_PATH/"
 
-git add --all .
+echo "Updating gh-pages branch..."
+
+git add --all . > /dev/null 2>&1
 git commit --allow-empty -m "Auto-update gh-pages from master branch"
 git push --force --quiet > /dev/null 2>&1
+
+echo "Done!"
