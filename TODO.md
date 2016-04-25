e2ba5bbd863474e79c3b2a93149157ef740bedc4
Alex Plescan <alexpls@gmail.com>
2016-04-25 19:53:39 +1000

    Fix up carousel sometimes not starting

--- TODO.md
+++ TODO.md
@@ -1,9 +1,7 @@
 Copy:
-  - Fix up copy on home page
   - Add meta description, title
   - Look up other SEO tags... do they need to be added in?
 
 Misc:
   - Add in download link to email template
-  - Figure out why sometimes the slideshow doesn't start in Safari
   - Add in fallback image for browsers that don't support CSS transforms
--- _assets/javascripts/carousel.js
+++ _assets/javascripts/carousel.js
@@ -2,6 +2,9 @@
 
   'use strict';
 
+  var MS_PER_SLIDE = 5000;
+  var MS_FOR_ANIM = 250;
+
   function Carousel(carouselElem) {
     var imageContainer = carouselElem.getElementsByClassName('images')[0];
     var slides = carouselElem.getElementsByTagName('img');
@@ -19,25 +22,23 @@
 
       console.log('showing slide' + currentSlideIdx);
 
-      imageContainer.style.marginLeft = (-(currentSlideIdx * slideWidth)) + 'px';
-
       for (var i = 0; i < slides.length; i++) {
         var slide = slides[i];
-        if (i === currentSlideIdx) {
-          (function(slide) {
-            setTimeout(function() {
-              slide.classList.add('active');
-            }, 250);
-          })(slide);
-        }
         slide.classList.remove('active');
+
+        if (i === currentSlideIdx) {
+          slide.classList.add('active');
+        }
       }
+
+      imageContainer.style.marginLeft = (-(currentSlideIdx * slideWidth)) + 'px';
     }
 
     function loadedSlide() {
       numSlidesLoaded++;
       if (numSlidesLoaded === numSlides) {
-        setInterval(nextSlide, 3000);
+        slideWidth = slides[0].width;
+        setInterval(nextSlide, MS_PER_SLIDE - MS_FOR_ANIM);
       }
     }
 
